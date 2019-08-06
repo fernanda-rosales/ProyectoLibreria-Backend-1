9fd62a8a0b6b72bb16e0497a39434a34dfb70a97
Galio666 <ascencio847@gmail.com>
2019-08-06 14:26:10 -0500

    Agregue Clases Tarjeta,publiccion,editorial,stock,categoria,valoracion

--- src/main/java/mx/edu/uacm/domain/Publicacion.java
+++ src/main/java/mx/edu/uacm/domain/Publicacion.java
@@ -8,26 +8,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class Publicacion {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private @Getter @Setter Long id;
+	private Long id;
 
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "publicacion")
-	private @Getter @Setter List<Libro> libros = new ArrayList<Libro>();
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "publicacion")
+	private List<Libro> libros = new ArrayList<Libro>();
 
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "publicacion")
-	private @Getter @Setter List<DetalleCompra> detalleCompra = new ArrayList<DetalleCompra>();
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "publicacion")
+	private List<DetalleCompra> detalleCompra = new ArrayList<DetalleCompra>();
 }
--- src/main/java/mx/edu/uacm/domain/Valoracion.java
+++ src/main/java/mx/edu/uacm/domain/Valoracion.java
@@ -8,26 +8,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class Valoracion {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private @Getter @Setter long id;
-	
-	private @Getter @Setter double valoracion;
-	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "valoracion")
-	private List<Libro> libros=new ArrayList<Libro>();
+	private long id;
+
+	private double valoracion;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "valoracion")
+	private List<Libro> libros = new ArrayList<Libro>();
 
 }
