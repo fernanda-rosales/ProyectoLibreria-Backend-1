8d387af899dc7080851b7569853056d2cb981125
Galio666 <ascencio847@gmail.com>
2019-08-06 14:18:51 -0500

    Agregue Clases Tarjeta,publiccion,editorial,stock,categoria,valoracion

--- src/main/java/mx/edu/uacm/domain/Libro.java
+++ src/main/java/mx/edu/uacm/domain/Libro.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,23 +16,34 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Libro {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private @Getter	@Setter Long id;
-	
-	private @Getter	@Setter int isbn;
-	
+	private @Getter @Setter Long id;
+
+	private @Getter @Setter int isbn;
+
 	@Column(length = 45)
-	private @Getter	@Setter String nombre;
-	
+	private @Getter @Setter String nombre;
+
 	@Column(length = 45)
-	private @Getter	@Setter String autor;
-	
+	private @Getter @Setter String autor;
+
 	@Column(length = 500)
-	private @Getter	@Setter String sipnosis;
-	
-	private @Getter	@Setter byte descatalogado;
-	
-	
+	private @Getter @Setter String sipnosis;
+
+	private @Getter @Setter byte descatalogado;
+
+	@ManyToOne
+	private @Getter @Setter Publicacion publicacion;
+
+	@ManyToOne
+	private @Getter @Setter Valoracion valoracion;
+
+	@ManyToOne
+	private @Getter @Setter Stock stock;
+
+	@ManyToOne
+	private @Getter @Setter Categoria categoria;
+
 }
--- src/main/java/mx/edu/uacm/domain/Publicacion.java
+++ src/main/java/mx/edu/uacm/domain/Publicacion.java
@@ -25,11 +25,9 @@ public class Publicacion {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private @Getter @Setter Long id;
 
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "Publiacion_idPublicacion")
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "publicacion")
 	private @Getter @Setter List<Libro> libros = new ArrayList<Libro>();
 
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "Publiacion_idPublicacion")
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "publicacion")
 	private @Getter @Setter List<DetalleCompra> detalleCompra = new ArrayList<DetalleCompra>();
 }
