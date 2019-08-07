25387aedbffb51fffccada03bc58f099e8070520
fernanda-rosales <chica_priston066@hotmail.com>
2019-08-07 12:43:42 -0500

    se completo metodo de pago ya con compra y contrarembolso

--- src/main/java/mx/edu/uacm/domain/MetodoDePago.java
+++ src/main/java/mx/edu/uacm/domain/MetodoDePago.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 import lombok.Data;
@@ -21,12 +22,23 @@ public class MetodoDePago {
 
 	//duda es forzoso el join column cuando uso onetomay y tendría problema con usar el cascadeType para este ejercicio
 	@OneToMany()
+	@JoinColumn(name="MetodoDePagoTransferenciaId")
 	private List<Transferencia> listaTransferencia=new ArrayList<Transferencia>();
 	
 	@OneToMany()
+	@JoinColumn(name="MetodoDePagoTarjetaId")
 	private List<Tarjeta> listaTarjeta=new ArrayList<Tarjeta>();
 	
 	@OneToMany()
+	@JoinColumn(name="MetodoDePagoPaypalId")
 	private List<Paypal> listaPaypal=new ArrayList<Paypal>();
+	
+	@OneToMany()
+	@JoinColumn(name="MetodoDePagoCompraId")
+	private List<Compra> listaCompra=new ArrayList<Compra>();
+	
+	@OneToMany()
+	@JoinColumn(name="MetodoDePagoContrarembolsoId")
+	private List<Contrarembolso> listaContra=new ArrayList<Contrarembolso>();
 	
 }
