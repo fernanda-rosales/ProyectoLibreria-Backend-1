eb2af5e1dd7075b8fa7f85492fde7b499681b39c
MartinezYonathan <j-ma@hotmail.es>
2019-08-07 12:08:47 -0500

    se quitaron etiquetas de error

--- src/main/java/mx/edu/uacm/domain/MetodoDePago.java
+++ src/main/java/mx/edu/uacm/domain/MetodoDePago.java
@@ -1,19 +1,13 @@
 package mx.edu.uacm.domain;
 
-<<<<<<< HEAD
-=======
 import java.util.ArrayList;
 import java.util.List;
 
->>>>>>> 61850437d337e36f5bedc3005238c13eac2a322c
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-<<<<<<< HEAD
-=======
 import javax.persistence.OneToMany;
->>>>>>> 61850437d337e36f5bedc3005238c13eac2a322c
 
 import lombok.Data;
 
@@ -24,8 +18,6 @@ public class MetodoDePago {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-<<<<<<< HEAD
-=======
 
 	//duda es forzoso el join column cuando uso onetomay y tendría problema con usar el cascadeType para este ejercicio
 	@OneToMany()
@@ -37,6 +29,4 @@ public class MetodoDePago {
 	@OneToMany()
 	private List<Paypal> listaPaypal=new ArrayList<Paypal>();
 	
-
->>>>>>> 61850437d337e36f5bedc3005238c13eac2a322c
 }
